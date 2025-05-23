b5721b8aab9dea3fde077deb9a4fa25f5661a248
zapwiz22 <kumar.jayant1916@gmail.com>
2025-05-23 12:23:01 +0530

    assign cookie leetcode prob

---
+++ greedy/assign-cookie.cpp
@@ -0,0 +1,23 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+  int findContentChildren(vector<int> &g, vector<int> &s) {
+    sort(g.begin(), g.end());
+    sort(s.begin(), s.end());
+    int i = 0, j = 0, cnt = 0;
+    while (i < g.size() and j < s.size()) {
+      if (g[i] <= s[j]) {
+        cnt++;
+        i++;
+        j++;
+      } else {
+        j++;
+      }
+    }
+    return cnt;
+  }
+};
