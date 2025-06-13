f0d19dc51779f3e0f90c0b4862915c957b315f6a
zapwiz22 <kumar.jayant1916@gmail.com>
2025-06-13 22:37:03 +0530

    count subsets with sum k

---
+++ dynamic-programming/17-count-subsets-with-sum-k.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<vector<int>> dp;
+int func(int index, int target, vector<int> &arr) {
+  if (target == 0)
+    return 1;
+  if (index == 0) {
+    return arr[0] == target;
+  }
+  int notTaken = func(index - 1, target, arr);
+  int taken = 0;
+  if (arr[index] <= target) {
+    taken = func(index - 1, target - arr[index], arr);
+  }
+  return taken + notTaken;
+}
+
+int findWays(vector<int> &arr, int k) {
+  int n = arr.size();
+  dp = vector<vector<int>>(n, vector<int>(k + 1, 0));
+  return func(n - 1, k, arr);
+}
+
+int main() {
+  vector<int> arr = {1, 2, 2, 3};
+  int k = 3;
+
+  cout << "The number of subsets found are " << findWays(arr, k);
+
+  return 0;
+}
