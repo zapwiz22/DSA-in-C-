9861d3b9e1a63d8b8fbab33a4114fe4691ff9f9f
zapwiz22 <kumar.jayant1916@gmail.com>
2025-06-14 17:33:45 +0530

    longest palindromic subsequence

---
+++ dynamic-programming/28-longest-palindromic-subsequence.cpp
@@ -0,0 +1,31 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int longestPalindromicSubsequence(string s) {
+  string rev = s;
+  reverse(rev.begin(), rev.end());
+  int n = s.length();
+  vector<int> prev(n + 1, 0), curr(n + 1, 0);
+  for (int i = 1; i <= n; i++) {
+    for (int j = 1; j <= n; j++) {
+      if (s[i] == rev[j]) {
+        curr[j] = 1 + prev[j - 1];
+      } else {
+        curr[j] = max(prev[j], curr[j - 1]);
+      }
+    }
+    prev = curr;
+  }
+  return prev[n];
+}
+
+int main() {
+  string s = "bbabcbcab";
+
+  // Call the longestPalindromeSubsequence function and print the result
+  cout << "The Length of Longest Palindromic Subsequence is "
+       << longestPalindromicSubsequence(s);
+  return 0;
+}
